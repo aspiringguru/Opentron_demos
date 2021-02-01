32b30b06e3b77c90af05b15cee4a787b6967d855
Pirate Patriot <sleepyhollowinoz@gmail.com>
2021-02-01 16:15:54 +1000

    file write removed

--- demo_new_5.py
+++ demo_new_5.py
@@ -25,13 +25,13 @@ metadata = {
 # protocol run function. the part after the colon lets your editor know
 # where to look for autocomplete suggestions
 
-def fill_rack(left_pipette, tipracks_, master, destination, control_1, control_2, master_offset_col, dest_name, f):
+def fill_rack(left_pipette, tipracks_, master, destination, control_1, control_2, master_offset_col, dest_name):
     #master_offset_col=0#master_offset_col move to function variables for algo simplicity.
     #dest_cols design does not need to be in function variables for this experiment design.
     dest_cols= [2, 22, 3,23]
     #
-    f.write("start "+dest_name+" ______________________________\n")
-    f.write("master_offset_col:"+str(master_offset_col)+"\n")
+    #f.write("start "+dest_name+" ______________________________\n")
+    #f.write("master_offset_col:"+str(master_offset_col)+"\n")
     #control1 from col2 to destination col2 and 22.
     for col_pos in dest_cols[0:2]:
         for offset_row in ["A","B"]:
@@ -45,7 +45,7 @@ def fill_rack(left_pipette, tipracks_, master, destination, control_1, control_2
             left_pipette.transfer(5, source.bottom(2), destination[well_pos].top(-2), new_tip='never')
             left_pipette.touch_tip()   #reinstate .touch step for production
             left_pipette.drop_tip()
-            f.write("control1: "+csv_string+"\n")
+            #f.write("control1: "+csv_string+"\n")
     #
     #control2 from col2 to destination col3 and 22.
     for col_pos in dest_cols[2:4]:
@@ -60,7 +60,7 @@ def fill_rack(left_pipette, tipracks_, master, destination, control_1, control_2
             left_pipette.transfer(5, source.bottom(2), destination[well_pos].top(-2), new_tip='never')
             left_pipette.touch_tip()   #reinstate .touch step for production
             left_pipette.drop_tip()
-            f.write("control2: "+csv_string+"\n")
+            #f.write("control2: "+csv_string+"\n")
     #
     #in previous simpler algo, pick from col 1 put to cols [4,5,6], then from col 2 to [7,8,9]
     # etc until pick from col 6 put to col [19,20,21]
@@ -82,17 +82,17 @@ def fill_rack(left_pipette, tipracks_, master, destination, control_1, control_2
                 well_pos = offset_row+str(col_pos*3+4+i)
                 left_pipette.pick_up_tip()
                 left_pipette.mix(2, 10)    #reinstate .mix step for production.
-                f.write("destination: "+source_address+","+well_pos+"\n")
+                #f.write("destination: "+source_address+","+well_pos+"\n")
                 left_pipette.transfer(5, source.bottom(2), destination[well_pos].top(-2), new_tip='never')
                 left_pipette.touch_tip()   #reinstate .touch step for production
                 left_pipette.drop_tip()
     #
-    f.write("end "+dest_name+" ______________________________\n")
+    #f.write("end "+dest_name+" ______________________________\n")
 
 
 def run(protocol):
     #
-    f = open('transfer_pos_records.csv','w')
+    #f = open('transfer_pos_records.csv','w')
     # labware
     microplate_name = 'corning_384_wellplate_112ul_flat'
     master 		= protocol.load_labware(microplate_name, '1')
@@ -127,21 +127,22 @@ def run(protocol):
     print("type(left_pipette):", type(left_pipette))
     #
     print("start dest plate 1")
-    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 0, "Destination plate 1", f)
+    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 0, "Destination plate 1")
     protocol.pause("Put a new plate in slot ?? and refill all of the tipracks.")
     #
     print("start dest plate 2")
     left_pipette.reset_tipracks()
-    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 6, "Destination plate 2", f)
+    #fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 6, "Destination plate 2", f)
+    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 6, "Destination plate 2")
     protocol.pause("Put a new plate in slot ?? and refill all of the tipracks.")
     #
     print("start dest plate 3")
     left_pipette.reset_tipracks()
-    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 12, "Destination plate 3", f)
+    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 12, "Destination plate 3")
     protocol.pause("Put a new plate in slot ?? and refill all of the tipracks.")
     #
     print("start dest plate 4")
     left_pipette.reset_tipracks()
-    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 18, "Destination plate 4", f)
+    fill_rack(left_pipette, tipracks_, master, destination1, control_1, control_2, 18, "Destination plate 4")
     #
-    f.close()
+    #f.close()
